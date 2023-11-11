79ba33b74c2de140b84ed89c8aaafd26c14b6f42
Gyeonga Koh <gykoh@c3r6s7.42seoul.kr>
2023-11-11 15:51:50 +0900

    fin

--- Makefile
+++ Makefile
@@ -6,7 +6,52 @@
 #    By: gykoh <gykoh@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/11/11 13:59:07 by gykoh             #+#    #+#              #
-#    Updated: 2023/11/11 13:59:08 by gykoh            ###   ########.fr        #
+#    Updated: 2023/11/11 15:51:30 by gykoh            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+NAME = push_swap
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
+HEADER = srcs/push_swap.h
+
+SRC = 	srcs/push_swap.c \
+		srcs/check_argv.c \
+		srcs/error_utils.c \
+		srcs/index.c \
+		srcs/libft_utils.c \
+		srcs/operations_p.c \
+		srcs/operations_r.c \
+		srcs/operations_rr.c \
+		srcs/operations_s.c \
+		srcs/parse.c \
+		srcs/rotate_cnt_a.c \
+		srcs/rotate_cnt_b.c \
+		srcs/rotate_type.c \
+		srcs/rotate.c \
+		srcs/sort_utils.c \
+		srcs/sort.c \
+		srcs/stack_utils.c \
+		srcs/stack.c \
+		
+OBJ = $(SRC:.c=.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	$(CC) -o $(NAME) $(OBJ)
+
+%.o: %.c ${HAEDER}
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	rm $(OBJ)
+
+fclean:
+	rm -f $(NAME) $(OBJ)
+
+re:
+	$(MAKE) fclean
+	$(MAKE) all
+
+.PHONY: all clean fclean re
--- srcs/rotate.c
+++ srcs/rotate.c
@@ -1,103 +1,103 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rotate_push.c                                      :+:      :+:    :+:   */
+/*   rotate.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gykoh <gykoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 20:52:08 by gykoh             #+#    #+#             */
-/*   Updated: 2023/11/11 05:09:45 by gykoh            ###   ########.fr       */
+/*   Updated: 2023/11/11 14:04:30 by gykoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	ft_do_rarb(t_stack **a, t_stack **b, int c, char s)
+int	ft_do_rarb(t_stack **a, t_stack **b, int data, char s)
 {
 	if (s == 'a')
 	{
-		while ((*a)->data != c && ft_find_location_b(*b, c) > 0)
+		while ((*a)->data != data && ft_find_location_b(*b, data) > 0)
 			ft_rr(a, b);
-		while ((*a)->data != c)
+		while ((*a)->data != data)
 			ft_ra(a);
-		while (ft_find_location_b(*b, c) > 0)
+		while (ft_find_location_b(*b, data) > 0)
 			ft_rb(b);
 		ft_pb(a, b);
 	}
 	else if (s == 'b')
 	{
-		while ((*b)->data != c && ft_find_location_a(*a, c) > 0)
+		while ((*b)->data != data && ft_find_location_a(*a, data) > 0)
 			ft_rr(a, b);
-		while ((*b)->data != c)
+		while ((*b)->data != data)
 			ft_rb(b);
-		while (ft_find_location_a(*a, c) > 0)
+		while (ft_find_location_a(*a, data) > 0)
 			ft_ra(a);
 		ft_pa(a, b);
 	}
 	return (-1);
 }
 
-int	ft_do_rrarrb(t_stack **a, t_stack **b, int c, char s)
+int	ft_do_rrarrb(t_stack **a, t_stack **b, int data, char s)
 {
 	if (s == 'a')
 	{
-		while ((*a)->data != c && ft_find_location_b(*b, c) > 0)
+		while ((*a)->data != data && ft_find_location_b(*b, data) > 0)
 			ft_rrr(a, b);
-		while ((*a)->data != c)
+		while ((*a)->data != data)
 			ft_rra(a);
-		while (ft_find_location_b(*b, c) > 0)
+		while (ft_find_location_b(*b, data) > 0)
 			ft_rrb(b);
 		ft_pb(a, b);
 	}
 	else if (s == 'b')
 	{
-		while ((*b)->data != c && ft_find_location_a(*a, c) > 0)
+		while ((*b)->data != data && ft_find_location_a(*a, data) > 0)
 			ft_rrr(a, b);
-		while ((*b)->data != c)
+		while ((*b)->data != data)
 			ft_rrb(b);
-		while (ft_find_location_a(*a, c) > 0)
+		while (ft_find_location_a(*a, data) > 0)
 			ft_rra(a);
 		ft_pa(a, b);
 	}
 	return (-1);
 }
 
-int	ft_do_rrarb(t_stack **a, t_stack **b, int c, char s)
+int	ft_do_rrarb(t_stack **a, t_stack **b, int data, char s)
 {
 	if (s == 'a')
 	{
-		while ((*a)->data != c)
+		while ((*a)->data != data)
 			ft_rra(a);
-		while (ft_find_location_b(*b, c) > 0)
+		while (ft_find_location_b(*b, data) > 0)
 			ft_rb(b);
 		ft_pb(a, b);
 	}
 	else if (s == 'b')
 	{
-		while (ft_find_location_a(*a, c) > 0)
+		while (ft_find_location_a(*a, data) > 0)
 			ft_rra(a);
-		while ((*b)->data != c)
+		while ((*b)->data != data)
 			ft_rb(b);
 		ft_pa(a, b);
 	}
 	return (-1);
 }
 
-int	ft_do_rarrb(t_stack **a, t_stack **b, int c, char s)
+int	ft_do_rarrb(t_stack **a, t_stack **b, int data, char s)
 {
 	if (s == 'a')
 	{
-		while ((*a)->data != c)
+		while ((*a)->data != data)
 			ft_ra(a);
-		while (ft_find_location_b(*b, c) > 0)
+		while (ft_find_location_b(*b, data) > 0)
 			ft_rrb(b);
 		ft_pb(a, b);
 	}
 	else if (s == 'b')
 	{
-		while (ft_find_location_a(*a, c) > 0)
+		while (ft_find_location_a(*a, data) > 0)
 			ft_ra(a);
-		while ((*b)->data != c)
+		while ((*b)->data != data)
 			ft_rrb(b);
 		ft_pa(a, b);
 	}
